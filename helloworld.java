f5598ad596136bd5f43f8ed5fea4f0545e2c6b58
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-04-26 15:31:05 +0900

    Drawing a circle

--- helloworld.java
+++ helloworld.java
@@ -1,17 +1,31 @@
 import java.awt.Frame;
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Color;
 
-public class helloworld
+// must inherit from Canvas and override "paint"
+// in order for this standard library feature to work
+public class helloworld extends Canvas
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
         final int WIDTH = 320;
         final int HEIGHT = 200;
-
-        Frame screen = new Frame("I'm a frame title");
-
-        screen.setBounds(40, 40, WIDTH, HEIGHT);
-        screen.setVisible(true);
         
-        System.out.println("hello there");
+        Frame window = new Frame("I'm a frame title");
+        Canvas screen = new helloworld();
+
+        screen.setSize(WIDTH, HEIGHT);
+        screen.setBackground(Color.pink);
+        
+        window.add(screen);
+        window.setBounds(40, 40, WIDTH, HEIGHT);
+        window.setVisible(true);
+    }
+
+    // overrides aren't marked as overrides?!
+    public void paint(Graphics g) {
+        g.setColor(new Color(125, 175, 255));
+        g.fillOval(130, 70, 50, 50);
     }
 }
