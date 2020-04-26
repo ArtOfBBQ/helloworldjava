f46c7b53dce16bddef06efaabcbaf97286980f80
Jelle Van den Eynde <40927974+ArtOfBBQ@users.noreply.github.com>
2020-04-26 13:42:37 +0900

    importing a standard library and showing a useless frame

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,17 @@
-package com.tok.helloworldjava;
+import java.awt.Frame;
 
 public class helloworld
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        final int WIDTH = 320;
+        final int HEIGHT = 200;
+
+        Frame screen = new Frame("I'm a frame title");
+
+        screen.setBounds(40, 40, WIDTH, HEIGHT);
+        screen.setVisible(true);
+        
+        System.out.println("hello there");
     }
 }
